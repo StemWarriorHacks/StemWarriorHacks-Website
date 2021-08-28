578133282a1b241c7d688533689ad1a80b783a10
kirtirajsinh <kirtiraj3738@gmail.com>
2021-08-28 17:45:54 +0530

    Add Navbar, Layout, Navbar.module.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import Meta from '../components/Meta'
+import Layout from "../components/Layout"
 import '../styles/globals.scss'
 
 const MyApp = ({ Component, pageProps }) => (
+	<Layout>
 	<>
 		<Meta />
 		<Component {...pageProps} />
 	</>
+	</Layout>
 )
 
 export default MyApp
