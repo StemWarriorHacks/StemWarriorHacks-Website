0b4f807271c2433fa1eb4cfddef8c0f796b1c3bf
Talla Giridhar <giridhar.talla2002@gmail.com>
2021-12-29 23:03:12 +0530

    Another sponsor

--- utils/data.js
+++ utils/data.js
@@ -172,10 +172,15 @@ module.exports = {
 			link: 'https://www.echo3d.co/',
 			image: '/IMG_8124.png',
 		},
-                {
+{
 			name: 'Symbl.ai',
 			link: 'https://symbl.ai/',
 			image: '/IMG_8125.png',
+		},
+{
+			name: '1Password',
+			link: 'https://1password.com/',
+			image: '/IMG_8126.png',
 		},
 	],
 	additionalSupport: [
